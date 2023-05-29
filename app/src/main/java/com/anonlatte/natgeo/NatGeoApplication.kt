b6561f3fef2879a88f54ac0e8b2905be671f187b
Herman Proshunin <g.proshunin15@gmail.com>
2023-05-29 20:35:07 +0500

    update libs

--- app/src/main/java/com/anonlatte/natgeo/NatGeoApplication.kt
+++ app/src/main/java/com/anonlatte/natgeo/NatGeoApplication.kt
@@ -1,12 +1,6 @@
 package com.anonlatte.natgeo
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.core.FlipperClient
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -16,12 +10,5 @@ class NatGeoApplication : Application() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
-        SoLoader.init(this, false)
-
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-            val client: FlipperClient = AndroidFlipperClient.getInstance(this)
-            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-            client.start()
-        }
     }
 }
--- app/src/main/java/com/anonlatte/natgeo/ui/custom/Core.kt
+++ app/src/main/java/com/anonlatte/natgeo/ui/custom/Core.kt
@@ -8,18 +8,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.ImagePainter
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.anonlatte.natgeo.R
 
 @ExperimentalCoilApi
 @Composable
 fun CoilImage(modifier: Modifier, data: Any?) {
-    val painter = rememberImagePainter(
-        data = data,
-        builder = { placeholder(R.drawable.whales) }
+    val painter = rememberAsyncImagePainter(
+        ImageRequest.Builder(LocalContext.current)
+            .data(data = data)
+            .apply { placeholder(R.drawable.whales) }
+            .build()
     )
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
         Image(
@@ -28,15 +32,21 @@ fun CoilImage(modifier: Modifier, data: Any?) {
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
-        if (painter.state is ImagePainter.State.Loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-        } else if (painter.state is ImagePainter.State.Error) {
-            Image(
-                modifier = modifier.fillMaxSize(),
-                painter = painterResource(R.drawable.placeholder_image),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-            )
+        when (painter.state) {
+            is AsyncImagePainter.State.Error -> {
+                Image(
+                    modifier = modifier.fillMaxSize(),
+                    painter = painterResource(R.drawable.placeholder_image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+            }
+
+            is AsyncImagePainter.State.Loading -> {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+
+            else -> Unit
         }
     }
 }
